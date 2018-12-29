7a5a1900f0415701adf184dfcbea495f585e3abd
Jon Gill <jon.gill@berkeley.edu>
2018-12-29 13:17:45 -0800

    Badges Continued

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -10,43 +13,43 @@ import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
+
 class App extends Component {
   render() {
     return (
       <div>
-            <div>
-              <Badge className={classes.margin} badgeContent={4} color="primary">
-                <MailIcon />
+        <Badge className={classes.margin} badgeContent={4} color="primary">
+          <MailIcon />
+        </Badge>
+        <Badge className={classes.margin} badgeContent={10} color="secondary">
+          <MailIcon />
+        </Badge>
+        <IconButton aria-label="4 pending messages" className={classes.margin}>
+          <Badge badgeContent={4} color="primary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+      </div>
+      <AppBar position="static" className={classes.margin}>
+        <Tabs value={0}>
+          <Tab
+            label={
+              <Badge className={classes.padding} color="secondary" badgeContent={4}>
+                Item One
               </Badge>
-              <Badge className={classes.margin} badgeContent={10} color="secondary">
-                <MailIcon />
-              </Badge>
-              <IconButton aria-label="4 pending messages" className={classes.margin}>
-                <Badge badgeContent={4} color="primary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-            </div>
-            <AppBar position="static" className={classes.margin}>
-              <Tabs value={0}>
-                <Tab
-                  label={
-                    <Badge className={classes.padding} color="secondary" badgeContent={4}>
-                      Item One
-                    </Badge>
-                  }
-                />
-                <Tab label="Item Two" />
-                <Tab label="Item Three" />
-              </Tabs>
-            </AppBar>
-            <Badge color="primary" badgeContent={4} className={classes.margin}>
-              <Typography className={classes.padding}>Typography</Typography>
-            </Badge>
-            <Badge color="primary" badgeContent={4} className={classes.margin}>
-              <Button variant="contained">Button</Button>
-            </Badge>
-          </div>
+            }
+          />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
+      <Badge color="primary" badgeContent={4} className={classes.margin}>
+        <Typography className={classes.padding}>Typography</Typography>
+      </Badge>
+      <Badge color="primary" badgeContent={4} className={classes.margin}>
+        <Button variant="contained">Button</Button>
+      </Badge>
+    </div>
     );
   }
 }
